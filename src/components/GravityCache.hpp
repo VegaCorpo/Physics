88e0e51d08f42e534e77685723fd5a165a13cd5d
Florian LABADIE <florian.labadie@epitech.eu>
2026-03-06 09:32:56 +0100

    style: format struct definitions for consistency in GravityCache.hpp

--- src/components/GravityCache.hpp
+++ src/components/GravityCache.hpp
@@ -3,18 +3,18 @@
 namespace physics {
     namespace components {
 
-    struct ScalarMass {
-        double value; // mantissa * 10^exponent
-    };
+        struct ScalarMass {
+                double value; // mantissa * 10^exponent
+        };
 
-    struct Displacement {
-        double dx;
-        double dy;
-        double dz;
-    };
+        struct Displacement {
+                double dx;
+                double dy;
+                double dz;
+        };
 
-    struct InverseDistance {
-        double invDistCubed;
-    };
+        struct InverseDistance {
+                double invDistCubed;
+        };
     } // namespace components
 } // namespace physics
