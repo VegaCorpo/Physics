845e950c485bf9a4e1736e2fa3e7eca5ad5555e4
Lucas <lucas.ludovic@epitech.eu>
2026-03-06 09:21:45 +0100

    feat: export as extern C instead of C++

--- src/PhysicsAPI.cpp
+++ src/PhysicsAPI.cpp
@@ -1,34 +1,43 @@
 #include "PhysicsAPI.hpp"
+#include <cstring>
+#include <iostream>
+#include "entt/entity/registry.hpp"
 #include "NewtonianPhysics.hpp"
 
-extern "C++" {
+extern "C" {
     std::string getName()
     {
         return physics::NewtonianPhysics::getName();
     }
 
-    void physicsInit(entt::registry& registry)
+    void physicsInit(void* registry_ptr)
     {
+        auto& registry = *static_cast<entt::registry*>(registry_ptr);
         physics::NewtonianPhysics::init(registry);
     }
 
-    void physicsUpdate(entt::registry& registry, double dt)
+    void physicsUpdate(void* registry_ptr, double dt)
     {
+        auto& registry = *static_cast<entt::registry*>(registry_ptr);
+        std::cout << "Hello from physics update" << std::endl;
         physics::NewtonianPhysics::update(registry, dt);
     }
 
-    void physicsShutdown(entt::registry& registry)
+    void physicsShutdown(void* registry_ptr)
     {
+        auto& registry = *static_cast<entt::registry*>(registry_ptr);
         physics::NewtonianPhysics::shutdown(registry);
     }
 
-    void physicsSyncIn(entt::registry& registry)
+    void physicsSyncIn(void* registry_ptr)
     {
+        auto& registry = *static_cast<entt::registry*>(registry_ptr);
         physics::NewtonianPhysics::syncIn(registry);
     }
 
-    void physicsSyncOut(entt::registry& registry)
+    void physicsSyncOut(void* registry_ptr)
     {
+        auto& registry = *static_cast<entt::registry*>(registry_ptr);
         physics::NewtonianPhysics::syncOut(registry);
     }
 }
